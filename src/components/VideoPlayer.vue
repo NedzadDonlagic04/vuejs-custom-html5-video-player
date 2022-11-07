fdad7ff960050ca2bac6978892e237b2a4b50cb7
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-11-07 10:41:13 +0100

    Added basic video element and set it on auto play

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="video-container">
+        <video class="video" src="./../assets/movie.mp4" controls autoplay></video>
+    </div>
+</template>
+
+<style scoped>
+    .video-container {
+        width: 100%;
+        max-width: 900px;
+    }
+
+    .video {
+        width: 100%;
+    }
+</style>
